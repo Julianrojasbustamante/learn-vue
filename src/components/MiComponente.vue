9fa6004b9e5d97aa06e4078ac961be864670e58b
Julián Rojas Bustamante <julianrjs15@gmail.com>
2022-06-28 09:44:17 -0500

    initial commit

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+    <h2>Los productos son:</h2>
+    <div v-for="product in productos">
+      <p>{{product.nombre}}</p>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "MiComponente",
+  data(){
+    return {
+      text: "Esto es un nuevo componente",
+      producto: {
+        descripcion: "",
+        estado: 1,
+        id: 1,
+        imagenPrincipal: "",
+        imagenes: [""],
+        nombre: "",
+        precio: 1,
+        unidadesDisponibles: 1
+      },
+      productos: []
+    }
+  },
+  created() {
+    axios.get("https://celuphone-b12a9-default-rtdb.firebaseio.com/datos.json").then((result) => {
+      console.log(result.data);
+      this.productos = result.data;
+    })
+  },
+  methods:{
+  }
+}
+</script>
